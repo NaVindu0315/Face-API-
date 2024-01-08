1f339e15a8947256b0eb9ab9cb9f5e476be8f56a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-09 01:14:33 +0530

    1

--- Server.js
+++ Server.js
@@ -2,3 +2,17 @@ const express = require('express');
 const request = require('request');
 const app = express();
 app.use(express.json());
+
+//for predefining the api key and secret
+const apiKey = 'Ihp7UgfV3b7KH-aAyQl5EiStwGX5ch1B';
+const apiSecret = '_kjlV-L5QjSYp9vQVP9a4VHosyehnbJ7';
+
+app.post('/compareFaces', (req, res) => {
+    const {  faceurl1, faceurl2 } = req.body;
+
+    const formData = {
+        api_key: apiKey,
+        api_secret: apiSecret,
+        image_url1: faceurl1,
+        image_url2: faceurl2,
+    };
